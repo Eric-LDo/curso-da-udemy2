a2d912a10b2919a6dad3c560b69c20a437d71b2e
Eric-LDo <121363351+Eric-LDo@users.noreply.github.com>
2023-12-21 09:42:03 -0300

    primeira aula

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import{text, view} from "react-native";
+const App = () =>{
+    return(
+        <SafeAreaView>
+            <text>Testando</text>
+        </SafeAreaView>
+    )
+
+};
+export default App;
